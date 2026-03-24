76bb8e6a0b3bf23c93ce8b8365a2aea433cce674
dithier <carter.ithier@gmail.com>
2026-03-24 10:42:39 -0400

    make longer job 8 hour instead of 4 request on hpc cluster in batch script

--- adam_baseline_v2.sh
+++ adam_baseline_v2.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH -p courses-gpu
-#SBATCH --gres=gpu:p100:1
-#SBATCH --time=04:00:00
+#SBATCH -p gpu
+#SBATCH --gres=gpu:v100-sxm2:1
+#SBATCH --time=8:00:00
 #SBATCH --job-name=baseline_training
-#SBATCH --output=logs/%j.out
-#SBATCH --error=logs/%j.err
+#SBATCH --output=logs/baseline_v2_adam/%j.out
+#SBATCH --error=logs/baseline_v2_adam/%j.err
 
 module load cuda/12.1.1 anaconda3/2024.06
 source activate cs7150
 
-python adam_baseline_training_v2.py \
+python -u adam_baseline_training_v2.py \
 	--epochs 30 \
 	--checkpoint False \
 	--dataset_dir flickr8k \
